fa2c37d98a290a170356f336fb4de0ab621a6f48
Ariel Lee <leea10@rpi.edu>
2016-10-07 17:28:50 -0400

    Timers work! Timer text is formatted

--- js/drinkOrder.js
+++ js/drinkOrder.js
@@ -1,27 +1,26 @@
 DrinkOrder = function(game, x, y, timeLimit) {
     Phaser.Sprite.call(this, game, x, y);
 
-    this.timer_ = new Phaser.Timer(game, /* autoDestroy */ false);
+    this.timer_ = game.time.create(false);
     this.signal_ = new Phaser.Signal();
 
-    this.timerText_ = this.addChild(new Phaser.Text(game, x, y, timeLimit.toString(), {
+    this.timerText_ = this.addChild(new Phaser.Text(game, x, y, util.formatTime(timeLimit), {
         align: 'center',
         fill: 'white',
     }));
-    this.timer_.add(timeLimit, function() {
-        console.log('wtf');
-    });
+    this.timer_.add(timeLimit, this.onTimerEnd, this);
     this.timer_.start();
-    console.log(this.timer_);
     // Populate with drink order requirements when we figure out how we're representing that.
 };
 DrinkOrder.prototype = Object.create(Phaser.Sprite.prototype); /* Do we make this a sprite group? */
 DrinkOrder.prototype.constructor = DrinkOrder;
 
 DrinkOrder.prototype.update = function() {
-    Phaser.Sprite.prototype.update.call(this);
-    console.log(this.timer_.duration);
-    this.timerText_.text = this.timer_.duration;
+    var timeLeft = this.timer_.duration;
+    if (timeLeft < 5000) {
+        this.timerText_.text.fillColor = 'red';
+    }
+    this.timerText_.text = util.formatTime(this.timer_.duration);
 };
 
 DrinkOrder.prototype.onTimerEnd = function() {
