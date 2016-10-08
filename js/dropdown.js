67c7d023fbdfefea89048e28f426d29024b0c3fc
Ariel Lee <ariellee616@gmail.com>
2016-10-08 01:59:12 -0400

    Menu slides up and down when orders are added/removed.

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,6 +4,7 @@
 var TAB_SIZE = 42;
 var SPACING = 81;
 var TWEEN_TIME = 333;
+var ORDER_TIME = 20000; /* For testing purposes */
 
 dropdown = function(game, x, y){
     Phaser.Sprite.call(this, game, x, y, 'dropdownImage');
@@ -16,7 +17,7 @@ dropdown = function(game, x, y){
     this.events.onInputDown.add(dropdown.onTap);
 
     // Custom properties
-    this.extended = false;
+    this.open_ = false;
     this.maxOrders_ = 5;
     this.activeOrders_ = [];
 };
@@ -37,18 +38,21 @@ dropdown.prototype.numOrders = function() {
 
 dropdown.prototype.addOrder = function() {
     var numOrders = this.numOrders();
-    if(numOrders >= 5) {
+    if(numOrders >= this.maxOrders_) {
         return;
     }
-    // TODO(Ariel): Instead of having the sprite spawn at 10,10, have it spawn in the menu on top of all the other orders.
-    // TODO(Ariel): Un-hardcode the amount of time we get for an order - currently at 2 minutes.
-    var newOrder = new DrinkOrder(this.game, 0, -(TAB_SIZE + SPACING * (numOrders + 1)), 20000);
+    var newOrder = new DrinkOrder(this.game, 0, -(TAB_SIZE + SPACING * (numOrders + 1)), ORDER_TIME);
     newOrder.anchor.set(0.5, 0);
     newOrder.addEvent(function() {
         this.removeOrder(newOrder);
     }, this);
     this.addChild(newOrder);
     this.activeOrders_.push(newOrder);
+
+    // If the menu is currently open, slide it down to reveal the newly added order.
+    if(this.open_) {
+        this.open();
+    }
 };
 
 /**
@@ -60,29 +64,43 @@ dropdown.prototype.removeOrder = function(order) {
         return;
     }
     this.activeOrders_.splice(index, 1);
-    // Relocate all the other orders' positions
+
+    // Shift all the other orders' positions to fill the gap.
     for(var i = index; i < this.numOrders(); i++) {
         this.activeOrders_[i].y = -(TAB_SIZE + SPACING * (i+1));
     }
+
+    // If the menu is currently open, slide it up to hide the empty slot.
+    if(this.open_) {
+        this.open();
+    }
 };
 
+dropdown.prototype.toggle = function() {
+    this.open_ ? this.close() : this.open();
+}
+
 /**
- * Slides open the order dropdown.
  * @param openNumber number of orders to show.
  * If undefined, the menu will open to however many orders there are.
  * If there are also no active orders, the menu will only slide to one space.
  */
-dropdown.prototype.slide = function(openNumber) {
+dropdown.prototype.open = function(openNumber) {
     openNumber = openNumber === undefined ? this.numOrders() : openNumber;
     openNumber = openNumber === 0 ? 1 : openNumber;
-    var goal = this.extended ? TAB_SIZE : SPACING * openNumber + TAB_SIZE;
+    var goal = SPACING * openNumber + TAB_SIZE;
     this.game.add.tween(this).to({y: goal}, TWEEN_TIME, Phaser.Easing.Cubic.InOut, true);
-    this.extended = !this.extended;
+    this.open_ = true;
+};
+
+dropdown.prototype.close = function() {
+    this.game.add.tween(this).to({y: TAB_SIZE}, TWEEN_TIME, Phaser.Easing.Cubic.InOut, true);
+    this.open_ = false;
 };
 
 /**
  * Event handler for tapping the dropdown.
  */
 dropdown.onTap = function(sprite, _) {
-    sprite.slide();
+    sprite.toggle();
 };
